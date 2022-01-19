4b4c5ddb63e39364886231535fa1d4084151425c
Yerzhan Kaiyrbek <kairbek_erzhan@mail.ru>
2022-01-20 03:35:12 +0600

    CreateCommonAndAuthorsUsers

--- sign/models.py
+++ sign/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
 
 
 class BaseRegisterForm(UserCreationForm):
@@ -16,3 +18,12 @@ class BaseRegisterForm(UserCreationForm):
                   "email",
                   "password1",
                   "password2", )
+
+class BasicSignupForm(SignupForm):
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        # Важно!!! Переменная basic_group (ниже), аргумент name='basic' должен соответствовать названию группы в джанге
+        basic_group = Group.objects.get(name='common')
+        # через атрибут user_set, возвращающий список всех пользователей этой группы, мы добавляем нового пользователя в эту группу
+        basic_group.user_set.add(user)
+        return user
